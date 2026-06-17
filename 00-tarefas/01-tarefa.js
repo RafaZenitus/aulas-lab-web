01d43bfb0f74c3ec82267d3a05944a9ce9554570
RafaZenitus <rafazenitus@gmail.com>
2026-06-17 13:55:44 -0300

    feat: add primeira tarefa

---
+++ 00-tarefas/01-tarefa.js
@@ -0,0 +1,14 @@
+/*
+- Crie um novo projeto Node.js
+
+- No arquivo crie duas variáveis e imprima o resultado da soma
+
+- Execute o arquivo e verifique a resposta do terminal
+
+*/
+
+const a = 10
+
+const b = 20
+
+console.log("Resultado da Soma: %d + %d = %d", a, b, a + b);
